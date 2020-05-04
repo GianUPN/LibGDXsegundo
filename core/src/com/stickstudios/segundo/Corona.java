f2f07db1e84c0b3cb10f427f98f56b5f88f85ae0
GiancarloCP <giancarlocalderonp@gmail.com>
2020-05-04 01:20:51 -0500

    TouchDown ejemplo y creación de objeto personaje.
    Signed-off-by: GiancarloCP <giancarlocalderonp@gmail.com>

---
+++ core/src/com/stickstudios/segundo/Corona.java
@@ -0,0 +1,86 @@
+package com.stickstudios.segundo;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+
+public class Corona extends InputAdapter {
+
+    Texture img;
+    Rectangle rectangle;
+    Vector2 posision;
+    ShapeRenderer renderer;
+    SpriteBatch batch;
+    Viewport viewport;
+
+    public Corona(Viewport viewport) {
+        this.viewport = viewport;
+        img = new Texture("corona.png");
+        posision = new Vector2(0, 0);
+        rectangle = new Rectangle(posision.x, posision.y, img.getWidth(), img.getHeight());
+        batch = new SpriteBatch();
+        renderer = new ShapeRenderer();
+    }
+
+    public void render(float delta) {
+        handleInput(delta);
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        renderer.setProjectionMatrix(viewport.getCamera().combined);
+        renderer.begin(ShapeRenderer.ShapeType.Line);
+        renderer.setColor(Color.WHITE);
+        renderer.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        renderer.end();
+        batch.begin();
+        batch.draw(img,posision.x,posision.y);
+        batch.end();
+    }
+
+    public void handleInput(float delta) {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            posision.x += delta * 180;
+            rectangle.x += delta * 180;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            posision.x -= delta * 180;
+            rectangle.x -= delta * 180;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            posision.y += delta * 180;
+            rectangle.y += delta * 180;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            posision.y -= delta * 180;
+            rectangle.y -= delta * 180;
+        }
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        Vector2 worldClick = viewport.unproject(new Vector2(screenX, screenY));
+        if(rectangle.contains(worldClick.x,worldClick.y)){
+            System.out.println("Click en corona xd");
+        }
+        return true;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return true;
+    }
+
+    public void dispose(){
+        img.dispose();
+        batch.dispose();
+        renderer.dispose();
+    }
+}
--- core/src/com/stickstudios/segundo/PantallaJuego.java
+++ core/src/com/stickstudios/segundo/PantallaJuego.java
@@ -1,87 +1,90 @@
 package com.stickstudios.segundo;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class PantallaJuego implements Screen {
 
-    SpriteBatch batch;
-    Texture img;
-    Rectangle rectangle;
+    public static final float WIDTH = 480.0f;
+    public static final float HEIGH = 360.0f;
+
+    Game game;
     Rectangle rectangle2;
     ShapeRenderer renderer;
-    Vector2 posision;
+    SpriteBatch batch;
     BitmapFont font;
+    Corona corona;
     String xd;
+    OrthographicCamera camera;
+    Viewport viewport;
+
+    public PantallaJuego(Game game){
+        this.game = game;
+    }
 
     @Override
     public void show() {
-        batch = new SpriteBatch();
-        img = new Texture("corona.png");
-        posision = new Vector2(0,0);
-        rectangle = new Rectangle(posision.x,posision.y,img.getWidth(),img.getHeight());
+        //camera = new OrthographicCamera();
+        viewport = new StretchViewport(WIDTH,HEIGH);
         rectangle2 = new Rectangle(100,100,50,50);
         renderer = new ShapeRenderer();
         font = new BitmapFont();
-        font.getData().setScale(3);
+        font.getData().setScale(2);
+        font.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        batch = new SpriteBatch();
+        corona = new Corona(viewport);
         xd= "nada";
+        Gdx.input.setInputProcessor(corona);
     }
 
     @Override
     public void render(float delta) {
+        viewport.apply();
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        /**
+         * No olvidar el projection matrix o de lo contrario
+         *  las coordenadas no seran las correctas.
+         */
+        renderer.setProjectionMatrix(viewport.getCamera().combined);
+        batch.setProjectionMatrix(viewport.getCamera().combined);
 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            posision.x += delta * 80;
-            rectangle.x  += delta * 80;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            posision.x -= delta * 80;
-            rectangle.x -= delta * 80;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            posision.y += delta * 80;
-            rectangle.y += delta * 80;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            posision.y -=delta * 80;
-            rectangle.y -= delta * 80;
-        }
+        renderer.begin(ShapeRenderer.ShapeType.Line);
+        renderer.setColor(Color.RED);
+        renderer.rect(rectangle2.x,rectangle2.y,50,50);
+        renderer.end();
+        batch.begin();
+        font.draw(batch,xd,100,40);
+        batch.end();
 
-        if(rectangle.overlaps(rectangle2)){
-            xd= "si";
+        if(corona.rectangle.overlaps(rectangle2)){
+            xd = "si";
         }else{
             xd = "nada";
         }
-
-
-        renderer.begin(ShapeRenderer.ShapeType.Line);
-        renderer.setColor(Color.WHITE);
-        renderer.rect(rectangle.x,rectangle.y,rectangle.width,rectangle.height);
-        renderer.end();
-        renderer.begin(ShapeRenderer.ShapeType.Line);
-        renderer.setColor(Color.RED);
-        renderer.rect(rectangle2.x,rectangle2.y,rectangle.width,rectangle.height);
-        renderer.end();
-        batch.begin();
-        batch.draw(img, posision.x, posision.y);
-        font.draw(batch,xd,100,40);
-        batch.end();
+        corona.render(delta);
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height, true);
     }
 
     @Override
@@ -102,8 +105,8 @@ public class PantallaJuego implements Screen {
     @Override
     public void dispose() {
         batch.dispose();
-        img.dispose();
         font.dispose();
         renderer.dispose();
+        corona.dispose();
     }
 }
